1c6c889aa2b595155bc4a3849d92f89f60e6ba8d
sujitbidawe <bidawesujit@gmail.com>
2020-03-13 09:58:27 +0530

    added: UC4employeeWageUsingSwitchCase  refactored

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,19 +1,21 @@
-#! /bin/bash
+#! /bin/bash -x
 
 isPartTime=1;
 isFullTime=2;
 empRatePerHour=20;
 randomCheck=$(( RANDOM%3 ));
 
-if [ $randomCheck -eq $isFullTime ];
-then
-		empHours=8;
-elif [ $randomCheck -eq $isPartTime ];
-then
-		empHours=4;
-else
-		empHours=0;
-fi
+case $randomCheck in
+		$isFullTime)
+				empHours=8
+				;;
+		$isPartTime)
+				empHours=4
+				;;
+		*)
+				empHours=0
+				;;
+esac
 
 salary=$(( $empHours*$empRatePerHour ));
 echo salary is $salary;
