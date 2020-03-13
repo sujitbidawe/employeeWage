8a033bc5d2b2fe458c99ef5867bd651c435adb42
sujitbidawe <bidawesujit@gmail.com>
2020-03-13 10:30:03 +0530

    added: UC5EmployeeWageUsingForLoop

--- employeeWage.sh
+++ employeeWage.sh
@@ -2,20 +2,28 @@
 
 isPartTime=1;
 isFullTime=2;
+totalSalary=0;
 empRatePerHour=20;
-randomCheck=$(( RANDOM%3 ));
+numWorkingDays=20;
+#randomCheck=$(( RANDOM%3 ));
 
-case $randomCheck in
-		$isFullTime)
-				empHours=8
-				;;
-		$isPartTime)
-				empHours=4
-				;;
-		*)
-				empHours=0
-				;;
-esac
+for (( days=1; days<=$numWorkingDays; days++ ))
+do
+	randomCheck=$(( RANDOM%3 ));
+			case $randomCheck in
+					$isFullTime)
+							empHours=8
+							;;
+					$isPartTime)
+							empHours=4
+							;;
+					*)
+							empHours=0
+							;;
+			esac
 
 salary=$(( $empHours*$empRatePerHour ));
+totalSalary=$(( $totalSalary+$salary ));
 echo salary is $salary;
+
+done
